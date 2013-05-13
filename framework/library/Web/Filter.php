b1fe0979a33e1d373b43f60147c8afa3363f86a8
xutongle <xutongle@gmail.com>
2013-05-14 00:14:03 +0800

    q
    Signed-off-by: xutongle <xutongle@gmail.com>

--- framework/library/Web/Filter.php
+++ framework/library/Web/Filter.php
@@ -7,17 +7,18 @@
  * @version $Id$
  */
 class Web_Filter {
-	private $_allowtags = 'p|br|b|strong|hr|a|img|object|param|form|input|label|dl|dt|dd|div|font';
-	private $_allowattrs = 'id|class|align|valign|src|border|href|target|width|height|title|alt|name|action|method|value|type';
-	private $_disallowattrvals = 'expression|javascript:|behaviour:|vbscript:|mocha:|livescript:';
+
+	private static $_allowtags = 'p|br|b|strong|hr|a|img|object|param|form|input|label|dl|dt|dd|div|font';
+	private static $_allowattrs = 'id|class|align|valign|src|border|href|target|width|height|title|alt|name|action|method|value|type';
+	private static $_disallowattrvals = 'expression|javascript:|behaviour:|vbscript:|mocha:|livescript:';
 
 	public function __construct($allowtags = null, $allowattrs = null, $disallowattrvals = null) {
-		if ($allowtags) $this->_allowtags = $allowtags;
-		if ($allowattrs) $this->_allowattrs = $allowattrs;
-		if ($disallowattrvals) $this->_disallowattrvals = $disallowattrvals;
+		if ($allowtags) self::$_allowtags = $allowtags;
+		if ($allowattrs) self::$_allowattrs = $allowattrs;
+		if ($disallowattrvals) self::$_disallowattrvals = $disallowattrvals;
 	}
 
-	public function input($cleanxss = 1) {
+	public static function input($cleanxss = 1) {
 		if (get_magic_quotes_gpc ()) {
 			$_POST = new_stripslashes ( $_POST );
 			$_GET = new_stripslashes ( $_GET );
@@ -25,35 +26,35 @@ class Web_Filter {
 			$_REQUEST = new_stripslashes ( $_REQUEST );
 		}
 		if (! defined ( 'IN_ADMIN' ) && $cleanxss) {
-			$_POST = $this->xss ( $_POST );
-			$_GET = $this->xss ( $_GET );
-			$_COOKIE = $this->xss ( $_COOKIE );
-			$_REQUEST = $this->xss ( $_REQUEST );
+			$_POST = self::xss ( $_POST );
+			$_GET = self::xss ( $_GET );
+			$_COOKIE = self::xss ( $_COOKIE );
+			$_REQUEST = self::xss ( $_REQUEST );
 		}
 	}
 
-	public function xss($string) {
+	public static function xss($string) {
 		if (is_array ( $string )) {
-			$string = array_map ( array ($this,'xss' ), $string );
+			$string = array_map ( array ('self','xss' ), $string );
 		} else {
 			if (strlen ( $string ) > 20) {
-				$string = $this->_strip_tags ( $string );
+				$string = self::_strip_tags ( $string );
 			}
 		}
 		return $string;
 	}
 
-	public function _strip_tags($string) {
-		return preg_replace_callback ( "|(<)(/?)(\w+)([^>]*)(>)|", array ($this,'_strip_attrs' ), $string );
+	public static function _strip_tags($string) {
+		return preg_replace_callback ( "|(<)(/?)(\w+)([^>]*)(>)|", array ('self','_strip_attrs' ), $string );
 	}
 
-	public function _strip_attrs($matches) {
-		if (preg_match ( "/^(" . $this->_allowtags . ")$/", $matches [3] )) {
+	public static function _strip_attrs($matches) {
+		if (preg_match ( "/^(" . self::$_allowtags . ")$/", $matches [3] )) {
 			if ($matches [4]) {
-				preg_match_all ( "/\s(" . $this->_allowattrs . ")\s*=\s*(['\"]?)(.*?)\\2/i", $matches [4], $m, PREG_SET_ORDER );
+				preg_match_all ( "/\s(" . self::$_allowattrs . ")\s*=\s*(['\"]?)(.*?)\\2/i", $matches [4], $m, PREG_SET_ORDER );
 				$matches [4] = '';
 				foreach ( $m as $k => $v ) {
-					if (! preg_match ( "/(" . $this->_disallowattrvals . ")/", $v [3] )) {
+					if (! preg_match ( "/(" . self::$_disallowattrvals . ")/", $v [3] )) {
 						$matches [4] .= $v [0];
 					}
 				}
