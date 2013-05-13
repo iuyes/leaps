d1bfec71d104f023ebf48edcfdfae8bbabaf80de
xutongle <xutongle@gmail.com>
2013-05-13 23:15:19 +0800

    清理文件夹准备升级2.0
    Signed-off-by: xutongle <xutongle@gmail.com>

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 网站入口
- * @author Tongle Xu <xutongle@gmail.com> 2012-12-24
- * @copyright Copyright (c) 2003-2103 www.tintsoft.com
- * @version $Id: index.php 2 2013-01-14 07:14:05Z xutongle $
- */
-require_once 'src/wekit.php';
-Wekit::run();
