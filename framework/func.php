67932313e39cdcf076eac5a2272f318d14d65e9e
xutongle <xutongle@gmail.com>
2013-06-27 17:32:56 +0800

    新版目录
    Signed-off-by: xutongle <xutongle@gmail.com>

---
+++ framework/func.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * 核心函数
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright &copy; 2009-2013 TintSoft Development Co., LTD
+ * @license http://www.tintsoft.com/html/about/copyright/
+ * @version $Id$
+ */
+define ( 'CORE_FUNCTION', true );
